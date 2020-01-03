934ba30619ee90c93d7db5560f88f1c613831f90
kimminki10 <pangshe10@gmail.com>
2020-01-03 13:27:58 +0900

    regiester

--- api/v1/auth/auth.ctrl.go
+++ api/v1/auth/auth.ctrl.go
@@ -1,19 +1,64 @@
 package auth
 
 import (
+	"io/ioutil"
 	"mingi/goyoma/database/models"
+	"mingi/goyoma/lib/common"
+	"os"
+	"time"
 
+	"github.com/dgrijalva/jwt-go"
 	"github.com/gin-gonic/gin"
 	"github.com/jinzhu/gorm"
+	"golang.org/x/crypto/bcrypt"
 )
 
 type User = models.User
 
+func hash(password string) (string, error) {
+	bytes, err := bcrypt.GenerateFromPassword([]byte(password), 12)
+	return string(bytes), err
+}
+
+func checkHash(password string, hash string) bool {
+	err := bcrypt.CompareHashAndPassword([]byte(hash), []byte(password))
+	return err == nil
+}
+
+func generateToken(data common.JSON) (string, error) {
+
+	//  token is valid for 7days
+	date := time.Now().Add(time.Hour * 24 * 7)
+
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.MapClaims{
+		"user": data,
+		"exp":  date.Unix(),
+	})
+
+	// get path from root dir
+	pwd, _ := os.Getwd()
+	keyPath := pwd + "/jwtsecret.key"
+
+	key, readErr := ioutil.ReadFile(keyPath)
+	if readErr != nil {
+		return "", readErr
+	}
+	tokenString, err := token.SignedString(key)
+	return tokenString, err
+}
+
 func register(c *gin.Context) {
 	db := c.MustGet("db").(*gorm.DB)
 	type RequestBody struct {
-		email string `json:"email" binding:"required"`
-		password
+		Email    string `json:"email" binding:"required"`
+		Password string `json:"password" binding:"required"`
+		UFName   string
+		ULName   string
+		Mobile   string
+		Height   float32
+		Weight   float32
+		Age      int
+		Gender   string
 	}
 
 	var body RequestBody
@@ -24,10 +69,39 @@ func register(c *gin.Context) {
 
 	// check existancy
 	var exists User
-	if err := db.Where("email = ?", body.email).First(&exists).Error; err == nil {
+	if err := db.Where("email = ?", body.Email).First(&exists).Error; err == nil {
 		c.AbortWithStatus(409)
 		return
 	}
 
 	hash, hashErr := hash(body.Password)
+	if hashErr != nil {
+		c.AbortWithStatus(500)
+		return
+	}
+
+	// create user
+	user := User{
+		Email:        body.Email,
+		UFName:       body.UFName,
+		ULName:       body.ULName,
+		Mobile:       body.Mobile,
+		Height:       body.Height,
+		Weight:       body.Weight,
+		Age:          body.Age,
+		Gender:       body.Gender,
+		PasswordHash: hash,
+	}
+
+	db.NewRecord(user)
+	db.Create(&user)
+
+	serialized := user.Serialize()
+	token, _ := generateToken(serialized)
+	c.SetCookie("token", token, 60*60*24*7, "/", "", false, true)
+
+	c.JSON(200, common.JSON{
+		"user":  user.Serialize(),
+		"token": token,
+	})
 }
